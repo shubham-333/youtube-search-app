7710a4b0a97b62f1c80646257d63eaff802e73e8
Shubham Singla <shubhamsingla369@gmail.com>
2022-06-13 20:48:30 +0530

    api added

--- youtube_search/youtube_search/settings.py
+++ youtube_search/youtube_search/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'search',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+YOUTUBE_DATA_API_KEY = 'AIzaSyDwMn0J8_F0BxN3tULOYA4l8OkKhGeAfB4'
